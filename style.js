d46842ab831e5f1f76dec75e412ac9ffde930f11
ChuckNorris1943 <chucknorrisbruv@gmail.com>
2025-09-17 21:15:17 +0200

    Delete style.js

--- style.js
+++
@@ -1,9 +0,0 @@
-const emoji = "🕹️";
-const svg = `<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'>
-  <text y='50%' x='50%' dominant-baseline='middle' text-anchor='middle' font-size='80'>${emoji}</text>
-</svg>`;
-
-const link = document.createElement('link');
-link.rel = 'icon';
-link.href = 'data:image/svg+xml,' + encodeURIComponent(svg);
-document.head.appendChild(link);
